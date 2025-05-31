3e7995bbc9c566610c0f8223ed7550aa8ae29801
GuilhermeRamos96 <guilherme9odonto@gmail.com>
2025-05-31 20:35:36 -0300

    Update export_utils.py

--- modules/export_utils.py
+++ modules/export_utils.py
@@ -1,14 +1,23 @@
 from fpdf import FPDF
 import io
 
-def export_summary_to_pdf(study_type, responses):
+def export_summary_to_pdf(study_type, responses, info):
     pdf = FPDF()
     pdf.add_page()
     # Add UTF-8 font support
     pdf.add_font("DejaVu", "", "/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", uni=True)
     pdf.set_font("DejaVu", size=12)
     
-    pdf.multi_cell(0, 10, f"Resumo da Avaliação Crítica\nTipo de Estudo: {study_type}\n\n")
+    pdf.multi_cell(0, 10, f"Resumo da Avaliação Crítica\nTipo de Estudo: {study_type}\n")
+
+    pdf.set_font("DejaVu", "", 10)
+    pdf.multi_cell(0, 8, f"Examinador: {info.get('nome_examinador', '')}")
+    pdf.multi_cell(0, 8, f"Data da avaliação: {info.get('data_avaliacao', '')}")
+    pdf.multi_cell(0, 8, f"Título do artigo: {info.get('titulo_artigo', '')}")
+    pdf.multi_cell(0, 8, f"Autor do artigo: {info.get('autor_artigo', '')}")
+    pdf.multi_cell(0, 8, f"Ano do artigo: {info.get('ano_artigo', '')}")
+    pdf.ln(5)
+
 
     for i, item in enumerate(responses):
         pdf.add_font("DejaVu", "B", "/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf", uni=True)
